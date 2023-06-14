d8743e0a143e06d51ecd736a8aff50038b88c13e
Ivan Shagun <136569386+IvanShagun@users.noreply.github.com>
2023-06-14 15:43:33 +0400

    Add files via upload

---
+++ DZ.py
@@ -0,0 +1,35 @@
+# a=input("Введите число:")
+# print(int(a[0]) + int(a[1]) + int(a[2]))
+
+
+
+# a=int(input("Введите количество журавликов:"))
+# if a % 3 != 0:
+#     print("Не соответствует условию")
+# else:
+#     k=int(a / 3 * 2)
+
+#     p=int(a / 3 / 2)
+
+#     s=p
+
+#     print("Петя", p, "Катя", k, "Сережа", s)
+
+
+
+# a=input('Введите номер билета:')
+# if (int(a[0]) + int(a[1]) + int(a[2])) == (int(a[3]) + int(a[4]) + int(a[5])):
+#     print("yes")
+# else:
+#     print("no")
+
+
+
+# l=int(input("Введите длину шоколадки:"))
+# w=int(input("Введите ширину шоколадки:"))
+# d=int(input("Введите количество долек:"))
+# o=l*w-d
+# if o % l == 0 or o % w == 0:
+#     print("yes")
+# else:
+#     print("no")
